1ea085628782d305e81f33954a81160cec3f3dcf
Taha Ebrahimzadeh <taha13800111@gmail.com>
2023-12-18 01:23:19 +0330

    Add theme and remove default css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+// module
+import { CssBaseline, GlobalStyles, ThemeProvider, createTheme } from '@mui/material'
 // custom
-import './i18n/config';
+import DatePicker from './components/date-picker'
+import './i18n/config'
+import appTheme from './theme/theme'
+import appStyle from './theme/app-style'
 
-function App() {
-  return null
+const theme = createTheme(appTheme)
+
+function App() {    
+    return (
+        <>
+            <GlobalStyles styles={appStyle} />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <DatePicker />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default App
